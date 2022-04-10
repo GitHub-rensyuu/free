dacb79ab09f62ad8f155eab14346189a3892c0c2
EC2 Default User <ec2-user@ip-172-31-30-195.ec2.internal>
2022-04-10 14:04:55 +0000

    メール機能作成

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+
 require_relative "boot"
 
 require "rails/all"
@@ -10,6 +11,12 @@ module Bookers2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
