34ae3b8404fd2a8874dbf7ab889b3d75a22612c2
EC2 Default User <ec2-user@ip-172-31-30-195.ec2.internal>
2022-04-10 11:20:56 +0000

    グループ削除機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index,:show,:edit,:update]
+  resources :groups do
+    get "join" => "groups#join"
+    delete "all_destroy" => 'groups#all_destroy'
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
