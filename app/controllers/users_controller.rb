bc8482e00427ed0cff8bff3d7d62c7f8fa68a20f
EC2 Default User <ec2-user@ip-172-31-30-195.ec2.internal>
2022-04-11 06:41:05 +0000

    投稿数表示機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
     # @books = @user.books
     @book = Book.new
     @books = Book.where(user_id: @user.id)
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
