f5f782675483b0d45d2661a4a248760af5362d45
EC2 Default User <ec2-user@ip-172-31-30-195.ec2.internal>
2022-04-12 07:53:00 +0000

    いいね&コメント機能作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many:favorites,dependent: :destroy
+  has_many:book_comments,dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+    # favorites.exists?(user_id: user.id)
+  end
 
   def self.search(search_word)
     Book.where(['category LIKE ?', "#{search_word}"])
@@ -14,8 +21,5 @@ class Book < ApplicationRecord
   end
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
-
-
-
 
 end
